6bdca0dff18afd1eed7e748afa007a7bf550942a
saransh <bhardwajsaransh08@gmail.com>
2025-11-10 21:02:31 +0530

    Update settings.py with new configuration

--- rabitmqtest/settings.py
+++ rabitmqtest/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bhardwajsaransh08@gmail.com'
-EMAIL_HOST_PASSWORD = 'djnn vtoz hkdx pdbm'
+EMAIL_HOST_USER = 'your gmail'
+EMAIL_HOST_PASSWORD = 'your password '
